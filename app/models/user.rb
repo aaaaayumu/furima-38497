111ac7224299ad414c9274cef05d9bd8e1bc4b30
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-08 20:32:33 +0900

    バリデーションの編集（エラーメッセージの重複を回避）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :birth_date, presence: true
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', if: :password_errors_blank?
   
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
-    validates :first_name_kana
-    validates :last_name_kana
+  validates_format_of :last_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :last_name_kana_errors_blank?
+  validates_format_of :first_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :first_name_kana_errors_blanK?
+
+  def password_errors_blank?
+    errors[:password].blank?
+  end
+
+  def last_name_kana_errors_blank?
+    errors[:last_name_kana].blank?
+  end
+
+  def first_name_kana_errors_blanK?
+    errors[:first_name_kana].blank?
   end
   
 end
