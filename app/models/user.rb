57972f1503c4d2903dbec051110ea55883984d8f
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-09 18:41:20 +0900

    :last_nameと:first_nameのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,23 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', if: :password_errors_blank?
+
+  validates_format_of :last_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角ひらがな、全角カタカナ、漢字を使用してください', if: :last_name_errors_blank?
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角ひらがな、全角カタカナ、漢字を使用してください', if: :frist_name_errors_blank?
   
   validates_format_of :last_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :last_name_kana_errors_blank?
   validates_format_of :first_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :first_name_kana_errors_blank?
 
   def password_errors_blank?
     errors[:password].blank?
+  end
+
+  def last_name_errors_blank?
+    errors[:last_name].blank?
+  end
+
+  def frist_name_errors_blank?
+    errors[:first_name].blank?
   end
 
   def last_name_kana_errors_blank?
