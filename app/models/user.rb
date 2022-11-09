355913838899219e6dd8e060090220e2bf4ab0b1
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-09 16:09:19 +0900

    コードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', if: :password_errors_blank?
   
   validates_format_of :last_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :last_name_kana_errors_blank?
-  validates_format_of :first_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :first_name_kana_errors_blanK?
+  validates_format_of :first_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください', if: :first_name_kana_errors_blank?
 
   def password_errors_blank?
     errors[:password].blank?
@@ -20,7 +20,7 @@ class User < ApplicationRecord
     errors[:last_name_kana].blank?
   end
 
-  def first_name_kana_errors_blanK?
+  def first_name_kana_errors_blank?
     errors[:first_name_kana].blank?
   end
   
