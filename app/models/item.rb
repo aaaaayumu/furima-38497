8acff34f60dcd78705e707e630845f06c81b971a
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-10 21:22:11 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,16 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  validates :name, :description, :price, presence: true
+  validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,
+    numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { in: 300..9999999, message: "は(￥300~9,999,999)で入力してください" }, if: :price_errors_blank?
+  validates :image, presence: true, blob: { content_type: :image }
+
+  def price_errors_blank?
+    errors[:price].blank?
+  end
+
+  
 end
