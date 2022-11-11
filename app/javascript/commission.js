e35e530cc009482abd9304c862f98ac9c92cea54
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-11 13:14:42 +0900

    販売手数料、販売利益をjavascriptで表示

---
+++ app/javascript/commission.js
@@ -0,0 +1,12 @@
+const commission = () => {
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  itemPrice.addEventListener("keyup", () => {
+    addTaxPrice.innerHTML = Math.floor(itemPrice.value * 0.1);
+    profit.innerHTML = itemPrice.value - addTaxPrice.innerHTML;
+  });
+};
+
+
+window.addEventListener('load', commission);
