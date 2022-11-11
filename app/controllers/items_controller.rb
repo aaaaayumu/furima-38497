c9784d09573679a1d591c94f1d78924b5e67c2f3
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-11 14:54:47 +0900

    createアクションでparamsを保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :description, :category_id, :condition_id,
+      :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
