62ffcab5c2ef086a68c53c6f540280713f8423c6
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-11 14:55:56 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :name, :description, :price, presence: true
   validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,
     numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { in: 300..9999999, message: "は(￥300~9,999,999)で入力してください" }, if: :price_errors_blank?
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は(￥300~9,999,999)で入力してください" }, if: :price_errors_blank?
   validates :image, presence: true, blob: { content_type: :image }
 
   def price_errors_blank?
