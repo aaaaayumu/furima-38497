5f8424a0151c4cc7fc53bc2ba5c9448aa39c9c6b
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-17 22:20:19 +0900

    ordersコントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,9 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+  end
+end
