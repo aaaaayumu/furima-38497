07beca94e5fb37e627275dec8e5c24285f91e205
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-19 13:02:35 +0900

    リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_root, only: :index
 
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
       @order_shipping_address.save
@@ -21,11 +20,17 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 
   def move_to_root
-    return redirect_to root_path if current_user.id == Item.find(params[:item_id]).user.id
-    return redirect_to root_path if Item.find(params[:item_id]).order.present?
+    return redirect_to root_path if current_user.id == @item.user.id
+    return redirect_to root_path if @item.order.present?
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
