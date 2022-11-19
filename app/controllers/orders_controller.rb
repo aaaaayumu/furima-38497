ed45911ccfe7b34f40b6e7f526e005285e6550f2
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-19 12:20:32 +0900

    商品購入画面への遷移制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_root, only: :index
+
   def index
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
@@ -19,5 +22,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def move_to_root
+    return redirect_to root_path if current_user.id == Item.find(params[:item_id]).user.id
+    return redirect_to root_path if Item.find(params[:item_id]).order.present?
   end
 end
