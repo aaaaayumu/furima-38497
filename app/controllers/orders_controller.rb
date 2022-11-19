c8fd4af75a61b2bbcb5b2b8efe3832cdfd9547f7
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-19 11:41:51 +0900

    createアクションの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
